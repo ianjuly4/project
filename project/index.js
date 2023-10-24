81b8ae33c0f08c205ec2f9f168cec9bf05e4c7be
Ian July <ianjuly4@gmail.com>
2023-10-23 21:34:00 -0600

    border collie postman

--- project/index.js
+++ project/index.js
@@ -1,10 +1,36 @@
 //fetch("http://localhost:3000/project")
-  //.then((resp) => resp.json())
-  //.then((json) => json{}
+  //.then(resp => resp.json())
+  //.then(json => json{
+
+  //})
+//fetch("http://localhost:3000/project")
+  //.then(function (response) {
+    //return response.json();
+  //})
+  //.then(function () {
+    //const header = document.createElement("h1")
+    //document.body.append(header)
+    //header.style.margin = "CENTER"
+    //header.textContent = "WELCOME TO DOGGY DAYCARE"
+    //header.style.color = "BLUE";
+    //return shiba;
+  
+  //});
 
 const header = document.createElement("h1")
 document.body.append(header)
+header.style.margin = "CENTER"
 header.textContent = "WELCOME TO DOGGY DAYCARE"
 header.style.color = "BLUE"
-header.style.right = "100px"
 
+//const div1 = document.createElement("div")
+//document.body.append(div1)
+
+//const par = document.createElement("p")
+//document.div.append(par)
+
+//function shiba(img_url){
+  //let img1 = document.querySelector("img") 
+  //img1.src = img_url 
+//}
+//shiba("https://images.dog.ceo/breeds/shiba/shiba-14.jpg")

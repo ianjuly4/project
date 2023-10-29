2e04eb04d499d9d4ad618e1f1a6645bc4fb69948
Ian July <ianjuly4@gmail.com>
2023-10-29 16:53:50 -0600

    eventListener Border add

--- project/index.js
+++ project/index.js
@@ -8,11 +8,22 @@
   //eventListener = mouseover
   const ShibaMouseOver = document.querySelector(".Shiba");
   
-  ShibaMouseOver.addEventListener('mouseover', function() {
-   alert('Im kind of stubborn!');
-  });
+  //ShibaMouseOver.addEventListener('mouseover', function() {
+   //alert('Even though I am cute and cuddly I am not a big fan of cats');
+  //});
+
 
   //eventListener = click
+  const ShibaMouseClick = document.querySelector(".Shiba")
+  const BorderMouseClick = document.querySelector(".Border")
+
+  ShibaMouseClick.addEventListener('click', function(){
+    alert('If you would like Macey to go home with you today, please go to this link to fill out an adoption application')
+  })
+
+  BorderMouseClick.addEventListener('click', function(){
+    alert('If you would like Max to go home with you today, please go to this link to fill out an adoption application')
+  })
 
   //eventListner = form
 

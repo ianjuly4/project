7cecd41502d317547d758327cefe1547c99b41b8
Ian July <ianjuly4@gmail.com>
2023-11-04 13:43:00 -0600

    create index2 - copy paste from index

--- project/index.js
+++ project/index.js
@@ -6,47 +6,48 @@
   //});
 
   //eventListener = mouseover
-  const ShibuMouseOver = document.querySelector(".ShibuP")
+  //const ShibuMouseOver = document.querySelector(".ShibuP")
   
-  ShibuMouseOver.addEventListener('mouseover', function(){
-    alert("Fun Fact: Shiba Inus almost became extinct during WW2 from bombing raids and a highly contagious canine viral infection")
-  })
+  //ShibuMouseOver.addEventListener('mouseover', function(){
+    //alert("Fun Fact: Shiba Inus almost became extinct during WW2 from bombing raids and a highly contagious canine viral infection")
+  //})
   
   //eventListener = click
-  const ShibaMouseClick = document.querySelector(".Shiba")
-  const BorderMouseClick = document.querySelector(".Border")
-  const SamoyedMouseClick = document.querySelector(".Samoyed")
-  const HuskeyMouseClick = document.querySelector(".Huskey")
-  const PitbullMouseClick = document.querySelector(".Pitbull")
-  const DachshundMouseClick = document.querySelector(".Dachshund")
+  //const ShibaMouseClick = document.querySelector(".Shiba")
+  //const BorderMouseClick = document.querySelector(".Border")
+  //const SamoyedMouseClick = document.querySelector(".Samoyed")
+  //const HuskeyMouseClick = document.querySelector(".Huskey")
+  //const PitbullMouseClick = document.querySelector(".Pitbull")
+  //const DachshundMouseClick = document.querySelector(".Dachshund")
 
-  ShibaMouseClick.addEventListener('click', function(){
-    alert('If you would like Macey to go home with you today, please go to this link to fill out an adoption application')
-  })
+  //ShibaMouseClick.addEventListener('click', function(){
+    //alert('If you would like Macey to go home with you today, please go to this link to fill out an adoption application')
+  //})
 
-  BorderMouseClick.addEventListener('click', function(){
-    alert('If you would like Max to go home with you today, please go to this link to fill out an adoption application')
-  })
+  //BorderMouseClick.addEventListener('click', function(){
+    //alert('If you would like Max to go home with you today, please go to this link to fill out an adoption application')
+  //})
 
-  SamoyedMouseClick.addEventListener('click', function(){
-    alert('If you would like Anna and/or Elsa to go home with you today, please go to this link to fill out an adoption application')
-  })
+  //SamoyedMouseClick.addEventListener('click', function(){
+    //alert('If you would like Anna and/or Elsa to go home with you today, please go to this link to fill out an adoption application')
+  //})
   
-  HuskeyMouseClick.addEventListener('click', function(){
-    alert('If you would like Chase to go home with you today, please go to this link to fill out an adoption application')
-  })
+  //HuskeyMouseClick.addEventListener('click', function(){
+    //alert('If you would like Chase to go home with you today, please go to this link to fill out an adoption application')
+  //})
 
-  PitbullMouseClick.addEventListener('click', function(){
-    alert('If you would like Fenway to go home with you today, please go to this link to fill out an adoption application')
-  })
+  //PitbullMouseClick.addEventListener('click', function(){
+    //alert('If you would like Fenway to go home with you today, please go to this link to fill out an adoption application')
+  //})
 
-  DachshundMouseClick.addEventListener('click', function(){
-    alert('If you would like Milly to go home with you today, please go to this link to fill out an adoption application')
-  })  
+  //DachshundMouseClick.addEventListener('click', function(){
+    //alert('If you would like Milly to go home with you today, please go to this link to fill out an adoption application')
+  //})  
   //eventListner = form
 
   //eventListener = DOMContentLoaded
 
   //eventListner = alert
 
+  
   

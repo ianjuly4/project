3bebba14bbbb274b79c74bd5b09ccd8bd3fb3a53
Ian July <ianjuly4@gmail.com>
2023-11-08 17:53:51 -0700

    create DOMContentloaded to portray dogpictures

--- project/index.js
+++ project/index.js
@@ -5,5 +5,28 @@
   //.then(function () {
   //});
 
-  
+const dogPictures = {
+  husky : {
+    info : "hi",
+    imageUrl : "https://images.dog.ceo/breeds/husky/n02110185_248.jpg",
+  },
+  ShibaInu : {
+    info : "Hi! I'm Macey. My ancestors are from Japan, but Me, I was born here in Denver. I am only 2 years old, but dont let that fool you I am very strong. Even though I have this cute and cuddly appearance, I hope to be a great huntress one day.",
+    imageUrl : "https://images.dog.ceo/breeds/shiba/shiba-14.jpg",
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  let form = document.querySelector("#dog_form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    handleDogPictures(e.target.dog_type.value)
+  })
+})
+
+function handleDogPictures(dogs){
+  let p = document.createElement('p')
+  p.textContent = dogs
+  document.querySelector(".dogPicturesContainer").appendChild(p)
+}
 

488460720853198dc2a0f166d86c8518216fb9e4
Ian July <ianjuly4@gmail.com>
2023-11-08 22:49:37 -0700

    remove all push function and arrays

--- project/index.js
+++ project/index.js
@@ -1,35 +1,15 @@
-//fetch("http://localhost:3000/project")
-  //.then(function (response) {
-    //return response.json();
-  //})
-  //.then(function () {
-  //});
 
-const dogPictures = []
-const ShibaInu = []
-const Husky = []
-const Samoyed = []
-const BorderCollie = []
-const Dachshund = []
-const Pitbull = []
-const Terrier = []
- 
-dogPictures.push(ShibaInu, Husky, Samoyed, BorderCollie, Dachshund, Pitbull, Terrier)
-//console.log(dogPictures)
+fetch("http://localhost:3000/project")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function () {
+  });
+  
 
-function ShibaInuInput() {
-  const ShibaInu = [];
-  const div = document.createElement('div')
-  const img = document.createElement('img')
-    img.src = "https://images.dog.ceo/breeds/shiba/shiba-14.jpg"
-    img.appendChild(div)
-  const p = document.createElement("p")
-    p.textContent =  "Hi! I'm Macey. My ancestors are from Japan, but Me, I was born here in Denver. I am only 2 years old, but dont let that fool you I am very strong. Even though I have this cute and cuddly appearance, I hope to be a great huntress one day."
-    p.appendChild(div)
-  ShibaInu.push(div, img, p)
-}
-ShibaInuInput()   
 
+
+//submit event
 document.addEventListener("DOMContentLoaded", () => {
   let form = document.querySelector("#dog_form")
   form.addEventListener("submit", (e) => {
@@ -38,9 +18,18 @@ document.addEventListener("DOMContentLoaded", () => {
   })
 })
 
+
+//display below form
 function portrayPictures(dogs){
   let p = document.createElement('p')
-  p.textContent = dogs
+  p.textContent = (dogs = ShibaInu.json())
   document.querySelector(".dogPicturesContainer").appendChild(p)
 }
 
+//iteration function
+function picturesIteration(array){
+  for(const dog of dogPictures){
+    if(dog === array)
+    return array
+  }
+}

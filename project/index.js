2b9adf61fdaa38634149e7879774e3bbb65c1ba9
Ian July <ianjuly4@gmail.com>
2023-11-14 10:16:55 -0700

    fix inputValue

--- project/index.js
+++ project/index.js
@@ -1,20 +1,47 @@
 
+
+//fetch request
+function fetchData(event) {
+  fetch('http://localhost:3000/dogData')
+    .then(response => response.json())
+    .then(data => {
+      const dogType = event.target.dog_type.value; // Store the value in a variable
+      let foundDog = data.find(dogFinder);
+      console.log(foundDog);
+      portrayPictures(foundDog)
+
+      function dogFinder(dog) {
+        return dog.breed === dogType; // Use the variable in the dogFinder function
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);   
+    });
+}
+
+//create submit event
 const init = () => {
   const inputForm = document.querySelector("form");
 
   inputForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    const input = document.querySelector("#dog_type");
-
-    console.log(input.value);
-
-    fetch(`http://localhost:3000/dogData/${input.value}`)
-      .then(res => res.json())
-      .then(data => {
-
-
-    })
+    fetchData(event); // Pass the event object to the fetchData function
+    console.log(event.target.dog_type.value)
+    
   });
 };
 
 document.addEventListener("DOMContentLoaded", init);
+
+//create render function 
+function portrayPictures(dog) {
+  let div = document.createElement('div');
+  div.className = "div1";
+  let img = document.createElement('img'); // Create an <img> element
+  img.src = `${dog.imageUrl}`; // Set the source of the image
+  div.appendChild(img); // Append the image to the div
+  let p = document.createElement('p');
+  p.textContent = `${dog.greeting}`;
+  div.appendChild(p); 
+  document.querySelector(".dogPicturesContainer").appendChild(div);
+}
